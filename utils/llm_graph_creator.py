9e93562c33e6c7d8eb931b7a9b8a839076d2f58e
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-07-03 17:43:40 +0530

    feat: Add custom prompt

--- utils/llm_graph_creator.py
+++ utils/llm_graph_creator.py
@@ -1,9 +1,16 @@
 from langchain_experimental.graph_transformers import LLMGraphTransformer
-from langchain_experimental.graph_transformers.llm import _convert_to_graph_document
+from langchain_experimental.graph_transformers.llm import _convert_to_graph_document, UnstructuredRelation, examples
 from langchain_core.documents import Document
 from langchain_community.graphs.graph_document import GraphDocument, Node, Relationship
-from typing import Any, Dict, cast
+from typing import Any, Dict, cast, Optional, List
 from langfuse.callback import CallbackHandler
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    PromptTemplate,
+)
+from langchain_core.messages import SystemMessage
+from langchain_core.output_parsers import JsonOutputParser
 
 
 langfuse_handler = CallbackHandler(
@@ -67,3 +74,78 @@ class LLMGraphTransformerWithLogging(LLMGraphTransformer):
                 ]
 
         return GraphDocument(nodes=nodes, relationships=relationships, source=document)
+    
+
+    def create_unstructured_prompt(
+        self, node_labels: Optional[List[str]] = None, rel_types: Optional[List[str]] = None
+    ) -> ChatPromptTemplate:
+        node_labels_str = str(node_labels) if node_labels else ""
+        rel_types_str = str(rel_types) if rel_types else ""
+        base_string_parts = [
+            "You are a top-tier algorithm designed for extracting information in "
+            "structured formats to build a knowledge graph. Your task is to identify "
+            "the entities and relations requested with the user prompt from a given "
+            "text. You must generate the output in a JSON format containing a list "
+            'with JSON objects. Each object should have the keys: "head", '
+            '"head_type", "relation", "tail", and "tail_type". The "head" '
+            "key must contain the text of the extracted entity with one of the types "
+            "from the provided list in the user prompt.",
+            f'The "head_type" key must contain the type of the extracted head entity, '
+            f"which must be one of the types from {node_labels_str}."
+            if node_labels
+            else "",
+            f'The "relation" key must contain the type of relation between the "head" '
+            f'and the "tail", which must be one of the relations from {rel_types_str}.'
+            if rel_types
+            else "",
+            f'The "tail" key must represent the text of an extracted entity which is '
+            f'the tail of the relation, and the "tail_type" key must contain the type '
+            f"of the tail entity from {node_labels_str}."
+            if node_labels
+            else "",
+            "Attempt to extract as many entities and relations as you can. Maintain "
+            "Entity Consistency: When extracting entities, it's vital to ensure "
+            'consistency. If an entity, such as "John Doe", is mentioned multiple '
+            "times in the text but is referred to by different names or pronouns "
+            '(e.g., "Joe", "he"), always use the most complete identifier for '
+            "that entity. The knowledge graph should be coherent and easily "
+            "understandable, so maintaining consistency in entity references is "
+            "crucial.",
+            "IMPORTANT NOTES:\n- Don't add any explanation and text.",
+        ]
+        system_prompt = "\n".join(filter(None, base_string_parts))
+
+        system_message = SystemMessage(content=system_prompt)
+        parser = JsonOutputParser(pydantic_object=UnstructuredRelation)
+
+        human_prompt = PromptTemplate(
+            template="""Based on the following example, extract entities and 
+    relations from the provided text.\n\n
+    Use the following entity types, don't use other entity that is not defined below:
+    # ENTITY TYPES:
+    {node_labels}
+
+    Use the following relation types, don't use other relation that is not defined below:
+    # RELATION TYPES:
+    {rel_types}
+
+    Below are a number of examples of text and their extracted entities and relationships.
+    {examples}
+
+    For the following text, extract entities and relations as in the provided example.
+    {format_instructions}\nText: {input}""",
+            input_variables=["input"],
+            partial_variables={
+                "format_instructions": parser.get_format_instructions(),
+                "node_labels": node_labels,
+                "rel_types": rel_types,
+                "examples": examples,
+            },
+        )
+
+        human_message_prompt = HumanMessagePromptTemplate(prompt=human_prompt)
+
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [system_message, human_message_prompt]
+        )
+        return chat_prompt
