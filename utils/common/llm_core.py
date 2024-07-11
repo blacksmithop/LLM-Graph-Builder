03f593f085769dd699e3947d87afef9884cee813
blacksmithop <angstycoder101@gmaii.com>
2024-07-11 15:32:40 +0530

    feat: Add Ollama LLM, Embeddings support

--- utils/common/llm_core.py
+++ utils/common/llm_core.py
@@ -3,17 +3,38 @@ from os import getenv
 from dotenv import load_dotenv
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
-from langchain_openai import (AzureChatOpenAI, AzureOpenAI,
-                              AzureOpenAIEmbeddings)
 
 load_dotenv()
 
-# OpenAI Embeddings
-underlying_embeddings = AzureOpenAIEmbeddings(
-    azure_deployment=getenv("EMBEDDINGS_NAME"),
-)
-
 store = LocalFileStore("./cache/")
+
+llm_provider = getenv("LLM_PROVIDER")
+llm_name = getenv("LLM_NAME")
+embeddings_name = getenv("EMBEDDINGS_NAME")
+
+if llm_provider == "openai":
+    from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+
+    # OpenAI Completion
+    llm = AzureChatOpenAI(
+        deployment_name=llm_name,
+        temperature=0,
+        max_tokens=2000,
+    )
+
+    # OpenAI Chat
+    underlying_embeddings = AzureOpenAIEmbeddings(
+        azure_deployment=embeddings_name,
+    )
+
+elif llm_provider == "ollama":
+    from langchain_community.embeddings import OllamaEmbeddings
+    from langchain_community.llms.ollama import Ollama
+
+    base_url = getenv("OLLAMA_URL") or "http://localhost:11434"
+    llm = Ollama(base_url=base_url, model=llm_name)
+
+    underlying_embeddings = OllamaEmbeddings(base_url=base_url, model=embeddings_name)
 
 embeddings = CacheBackedEmbeddings.from_bytes_store(
     underlying_embeddings,
@@ -22,16 +43,6 @@ embeddings = CacheBackedEmbeddings.from_bytes_store(
     query_embedding_cache=True,
 )
 
-# OpenAI Completion
-llm = AzureOpenAI(deployment_name=getenv("DEPLOYMENT_NAME"), temperature=0)
-llm.max_tokens = 2000
-
-llm = AzureChatOpenAI(
-    deployment_name=getenv("LLM_DEPLOYMENT_NAME"),
-    temperature=0,
-    max_tokens=4000,
-)
-
 if __name__ == "__main__":
     print(embeddings.embed_query("Hi")[:5])
-    print(llm.invoke("Tell me a joke").content)
+    print(llm.invoke("Tell me a joke"))
