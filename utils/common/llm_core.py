361cd360a46659bb98f188de7e68c57a68df40e8
blacksmithop <angstycoder101@gmaii.com>
2024-07-11 09:55:27 +0530

    feat: Rename llm, embedding definition file; TODO: Add Ollama support

--- utils/common/llm_core.py
+++ utils/common/llm_core.py
@@ -26,26 +26,12 @@ embeddings = CacheBackedEmbeddings.from_bytes_store(
 llm = AzureOpenAI(deployment_name=getenv("DEPLOYMENT_NAME"), temperature=0)
 llm.max_tokens = 2000
 
-gpt4_llm = AzureChatOpenAI(
-    azure_endpoint=getenv("AZURE_OPENAI_GPT4_ENDPOINT"),
-    deployment_name=getenv("GPT4_DEPLOYMENT_NAME"),
-    api_key=getenv("GPT4_OPENAI_API_KEY"),
-    api_version=getenv("OPENAI_API_VERSION"),
+llm = AzureChatOpenAI(
+    deployment_name=getenv("LLM_DEPLOYMENT_NAME"),
     temperature=0,
     max_tokens=4000,
 )
 
-
-gpt3_llm = AzureChatOpenAI(
-    azure_endpoint=getenv("AZURE_OPENAI_GPT3_ENDPOINT"),
-    deployment_name=getenv("GPT3_DEPLOYMENT_NAME"),
-    api_key=getenv("GPT3_OPENAI_API_KEY"),
-    api_version=getenv("OPENAI_API_VERSION"),
-    temperature=0.4,
-    max_tokens=1000,
-)
-
 if __name__ == "__main__":
     print(embeddings.embed_query("Hi")[:5])
-    print(gpt3_llm.invoke("Tell me a joke").content)
-    print(gpt4_llm.invoke("Tell me a joke").content)
+    print(llm.invoke("Tell me a joke").content)
