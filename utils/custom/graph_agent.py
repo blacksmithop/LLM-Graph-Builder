2793a04dde04965f86aeb87029f5225745a9987e
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-07-12 11:08:47 +0530

    feat: Add v3 prompt

--- utils/custom/graph_agent.py
+++ utils/custom/graph_agent.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import JsonOutputParser
 
 from utils.common.llm_core import llm
 from utils.custom.models import UnstructuredRelationNodes
-from utils.custom.prompts import (BASE_PROMPT, EXAMPLE_PROMPT,
+from utils.custom.prompts import (BASE_PROMPT, CORRELATION_PROMPT, EXAMPLE_PROMPT,
                                   HEAD_TAIL_PROMPT, INSTRUCTION_PROMPT,
                                   NODE_RELATION_EXAMPLE_PROMPT, SCHEMA_PROMPT)
 
@@ -35,3 +35,25 @@ def get_graph_chain_v2(
     )
 
     return generate_graph_chain
+
+def get_graph_chain_v3(
+    node_labels: List[str] = allowed_nodes,
+    rel_types: List[str] = allowed_relations[30:60],
+    examples: List[Dict] = examples,
+):
+    human_prompt = PromptTemplate(
+        template=f"{BASE_PROMPT}\n{HEAD_TAIL_PROMPT}\n{CORRELATION_PROMPT}\n{INSTRUCTION_PROMPT}\n{NODE_RELATION_EXAMPLE_PROMPT}\n{EXAMPLE_PROMPT}\n{SCHEMA_PROMPT}",
+        input_variables=["input"],
+        partial_variables={
+            "schema": parser.get_format_instructions(),
+            "node_labels": node_labels,
+            "rel_types": rel_types,
+            "examples": examples,
+        },
+    )
+    generate_graph_chain = (
+        human_prompt | llm | JsonOutputParser(pydantic_object=UnstructuredRelationNodes)
+    )
+
+    return generate_graph_chain
+
