e656b9eb6ba1151a8185601e24c0ea0c443041e4
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-07-12 17:43:10 +0530

    feat: Allow custom example for v3

--- knowledge_graph_creator.py
+++ knowledge_graph_creator.py
@@ -3,11 +3,10 @@ import logging
 import coloredlogs
 from dotenv import load_dotenv
 
-from utils.common.constants import allowed_nodes, allowed_relations
+from utils.common.constants import allowed_nodes, allowed_relations, examples, examples_v3
 from utils.common.create_document import get_documents_from_df
 from utils.common.file_handler import LocalFileHandler
 from utils.custom.knowledge_graph import Neo4JKnowledgeGraph
-
 
 load_dotenv()
 coloredlogs.install(level="DEBUG")
@@ -28,10 +27,10 @@ df = file_handler.read_local_file(FILE_PATH)
 
 documents = get_documents_from_df(
     df=df, insight_column="Insight", id_column="InsightID"
-)
+)[:50]
 
 neo4j = Neo4JKnowledgeGraph(
-    document_name=FILE_NAME, rel_types=allowed_relations[:30], node_labels=allowed_nodes
+    document_name=FILE_NAME, rel_types=allowed_relations[:30], node_labels=allowed_nodes, examples=examples_v3, prompt_version=3
 )
 
 neo4j.create_knowledge_graph(documents=documents)
